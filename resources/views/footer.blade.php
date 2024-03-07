e94a5bdf1f80c0c1ca62082081bf30c6a84361f4
diego_c <curbelord@gmail.com>
2024-03-07 09:21:41 +0000

    Footer version movil

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Footer</title>
+    @vite('resources/js/app.js')
+    <link rel="stylesheet" href="{{ asset('build/assets/css/style.css') }}">
+</head>
+<body>
+    <footer>
+        <div id="container_footer">
+            <div id="container_rrss_footer">
+                <div id="icono_facebook" class="icono_rrss_footer"></div>
+                <div id="icono_instagram" class="icono_rrss_footer"></div>
+                <div id="icono_twitter" class="icono_rrss_footer"></div>
+                <div id="icono_youtube" class="icono_rrss_footer"></div>
+            </div>
+            <div id="container_logo_footer">
+                <div id="logo_footer"></div>
+            </div>
+            <div id="container_copyright_footer">
+                <p>© Next Job 2024. Todos los derechos reservados.</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
