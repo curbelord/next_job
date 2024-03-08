800c7e6c263203109e33ede187bba0dc0f6efb52
diego_c <curbelord@gmail.com>
2024-03-08 12:58:26 +0000

    Pagina informacion de proceso terminada. Pendiente adaptar tamanho fuentes pagina actual y descripcion de oferta

---
+++ resources/views/process_info.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Información de proceso')
+
+@section('style')
+    <link rel="stylesheet" href="{{ asset('build/assets/css/styleProcessInfo.css') }}">
+@endsection
+
+@section('content')
+    <div id="container">
+        <div id="container_datos_proceso">
+            <div id="container_imagen">
+                <div id="imagen_empresa"></div>
+            </div>
+            <div id="container_texto_datos">
+                <div id="titulo_oferta">
+                    <h1>Título oferta</h1>
+                </div>
+                <div id="nombre_empresa">
+                    <p>Nombre empresa</p>
+                </div>
+                <div id="centro_trabajo">
+                    <p>Centro de trabajo</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="container_ultimo_cambio">
+            <div id="subcontainer_ultimo_cambio">
+                <p>Último cambio</p>
+            </div>
+        </div>
+
+        <div id="container_cambios">
+            @for ($i = 0; $i < 3; $i++)
+                @component('cambio')
+                @endcomponent
+            @endfor
+
+            <div id="container_estado_candidatura">
+                <div id="titulo_estado_candidatura">
+                    <h3>Estado de la candidatura</h3>
+                </div>
+                <div id="mensaje_estado_candidatura">
+                    <p>Mensaje que describa la situación y anime al demandante</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
