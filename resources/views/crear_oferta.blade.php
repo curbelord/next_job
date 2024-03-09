944516a0049071204a1bafdd4f957b3d5f54d19e
Xerach <axercay@gmail.com>
2024-03-09 19:39:54 +0000

    Creación de vista Crear Oferta (No responsive)

--- resources/views/crear_oferta.blade.php
+++ resources/views/crear_oferta.blade.php
@@ -3,11 +3,27 @@
 @section('title', 'Crear oferta')
 
 @section('style')
-    <link rel="stylesheet" href="{{ asset('build/assets/css/styleEmpresaBuscada.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/css/styleCrearOferta.css') }}">
 @endsection
 
 @section('content')
 
-    <p>Esto es la vista de crear oferta</p>
+    <div class="crear_oferta">
+
+        <h2>Crear una oferta</h2>
+
+        <input type="text" name="titulo" id="titulo" placeholder="Título" require>
+        <input type="text" name="centro_trabajo" id="centro_trabajo" placeholder="Centro de trabajo">
+        <input type="text" name="jornada" id="jornada" placeholder="Jornada">
+        <input type="text" name="horario" id="horario" placeholder="Horario">
+        <input type="text" name="n_vacantes" id="n_vacantes" placeholder="Número de vacantes">
+        <input type="text" name="salario" id="salario" placeholder="Salario">
+        <input type="text" name="killer_questions" id="killer_questions" placeholder="Killer questions">
+
+        <button class="crear_oferta_boton">Crear</button>
+
+        <button class="guardar_plantilla_boton">Guardar como plantilla</button>
+
+    </div>
 
 @endsection
