26e1eba92282ef27d3a29ccbbabbad7bde972bae
diego <curbelord@gmail.com>
2024-03-09 11:07:35 +0000

    Pagina empresa buscada terminada full responsive. Componente ultima oferta creado y adaptado

---
+++ resources/views/components/ultima_oferta.blade.php
@@ -0,0 +1,19 @@
+<div class="container_oferta">
+    <div class="titulo_oferta">
+        <h3>Título oferta</h3>
+    </div>
+    <div class="container_centro_trabajo_fecha_publicacion">
+        <div class="centro_trabajo_oferta">
+            <p>Centro de trabajo</p>
+        </div>
+        <div class="fecha_publicacion_oferta">
+            <p>01/01/2001</p>
+        </div>
+    </div>
+    <div class="descripcion_oferta">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, rerum, provident excepturi dicta eos</p>
+    </div>
+    <div class="boton_ver_oferta">
+        <a href="#">Ver oferta</a>
+    </div>
+</div>
