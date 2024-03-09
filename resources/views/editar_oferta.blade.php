ed38d1c177c915df4d84827743dcc58c7e94e34c
Xerach <axercay@gmail.com>
2024-03-09 20:41:38 +0000

    Creación de las vistas Crear Plantilla y Editar Oferta

--- resources/views/editar_oferta.blade.php
+++ resources/views/editar_oferta.blade.php
@@ -9,22 +9,21 @@
 @section('content')
 
     <div class="content">
-            
+
         <div class="editar_oferta">
 
             <h2>Editar una oferta</h2>
 
             <input type="text" name="titulo" id="titulo" placeholder="Título" require>
             <input type="text" name="centro_trabajo" id="centro_trabajo" placeholder="Centro de trabajo">
+            <textarea name="descripcion" id="descripcion" cols="30" rows="10" placeholder="Descripción"></textarea>
             <input type="text" name="jornada" id="jornada" placeholder="Jornada">
             <input type="text" name="horario" id="horario" placeholder="Horario">
             <input type="text" name="n_vacantes" id="n_vacantes" placeholder="Número de vacantes">
             <input type="text" name="salario" id="salario" placeholder="Salario">
             <input type="text" name="killer_questions" id="killer_questions" placeholder="Killer questions">
 
-            <button class="editar_oferta">Crear</button>
-
-            <button class="guardar_como">Guardar como plantilla</button>
+            <button>Editar</button>
 
         </div>
     
