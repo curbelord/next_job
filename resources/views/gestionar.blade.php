5b49be65cd6c0ff4fb3cd60feda8bfc25c1a8bc3
diego_c <curbelord@gmail.com>
2024-03-11 09:53:52 +0000

    Pagina principal empresas finalizada. Pendiente responsive

--- resources/views/gestionar.blade.php
+++ resources/views/gestionar.blade.php
@@ -7,53 +7,82 @@
 @endsection
 
 @section('content')
+    <div id="container">
+        <div id="container_bienvenida_seleccionador">
+            <h3>Bienvenid@, NombreSeleccionador</h3>
+            <p>¿Qué quieres hacer?</p>
+        </div>
 
-    <div class="seccion_gestion">
-
-        <h2>
-            Bienvenid@
-        </h2>
-
-        <p>
-            ¿Qué quieres hacer?
-        </p>
-
-    </div>
-
-    <div class="seccion_gestion">
-
-        <div class="gestion gestion_1-3">
-            <div class="imagen_gestion">IMAGEN</div>
-            <div class="nombre_gestion">
-                <span>
-                    Gestionar ofertas
-                </span>
-                <div class="vector_borde_azul"></div>
+        <div id="container_seccion_gestion">
+            <div id="bloque_gestion_1" class="bloque_gestion">
+                <div class="imagen_gestion"></div>
+                <div class="texto_gestion">
+                    <a href="#">Publicar proceso</a>
+                </div>
+            </div>
+            <div id="bloque_gestion_2" class="bloque_gestion">
+                <div class="imagen_gestion"></div>
+                <div class="texto_gestion">
+                    <a href="#">Gestionar proceso</a>
+                </div>
+            </div>
+            <div id="bloque_gestion_3" class="bloque_gestion">
+                <div class="imagen_gestion"></div>
+                <div class="texto_gestion">
+                    <a href="#">Crear plantilla</a>
+                </div>
             </div>
         </div>
 
-        <div class="gestion gestion_2-3">
-            <div class="imagen_gestion">IMAGEN</div>
-            <div class="nombre_gestion"> 
-                <span>
-                    Gestionar plantillas
-                </span>
-                <div class="vector_borde_azul"></div>
-            </div>            
-        </div>
+        <div id="container_ultimos_procesos">
+            <div id="titulo_ultimos_procesos">
+                <h3>Últimos procesos</h3>
+            </div>
+            <div id="subcontainer_ultimos_procesos">
+                <div class="container_oferta">
+                    <div class="datos_top">
+                        <div class="titulo_oferta">
+                            <h3>Título oferta</h3>
+                        </div>
+                        <div class="centro_trabajo_fecha_publicacion">
+                            <div class="centro_trabajo">
+                                <p>Centro de trabajo</p>
+                            </div>
+                            <div class="fecha_publicacion">
+                                <p>Fecha publicación</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="datos_bottom">
+                        <div class="container_numero_inscritos">
+                            <div class="subcontainer_numero_inscritos">
+                                <div class="imagen_datos_bottom imagen_inscritos"></div>
+                                <div class="valor_numero_inscritos">
+                                    <p>Nº inscritos</p>
+                                </div>
+                            </div>
+                        </div>
 
-        <div class="gestion gestion_3-3">
-            <div class="imagen_gestion">IMAGEN</div>
-            <div class="nombre_gestion"> 
-                <span>
-                    Otra opción
-                </span>
-                <div class="vector_borde_azul"></div>
-            </div>            
-        </div>
+                        <div class="container_numero_preseleccionados">
+                            <div class="subcontainer_numero_preseleccionados">
+                                <div class="imagen_datos_bottom imagen_preseleccionados"></div>
+                                <div class="valor_numero_preseleccionados">
+                                    <p>Nº preseleccionados</p>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div class="container_numero_descartados">
+                            <div class="subcontainer_numero_descartados">
+                                <div class="imagen_datos_bottom imagen_descartados"></div>
+                                <div class="valor_numero_descartados">
+                                    <p>Nº descartados</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <div class="bloque"></div>
-
 @endsection
