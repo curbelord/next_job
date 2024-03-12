5aa88b611c5d48650e8bde7e7dafa40b4649bb11
Xerach <axercay@gmail.com>
2024-03-12 11:24:40 +0000

    Cambio en el archivo web.php

---
+++ app/Http/Controllers/GestionPlantillaController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Oferta;
+use App\Models\Seleccionador;
+use App\Models\Empresa;
+use App\Models\Inscripcion;
+
+use Illuminate\Http\Request;
+
+class GestionPlantillaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    { 
+        return view('crear_plantilla');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit()
+    {
+        return view('crear_plantilla');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
