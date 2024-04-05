cd03dcdc9a1733108013667d9d624e25a86cd6d8
diego <curbelord@gmail.com>
2024-04-05 19:30:02 +0100

    Gestion de procesos v8.2. Pagina principal finalizada

--- public/build/assets/js/principal_procesos.js
+++ public/build/assets/js/principal_procesos.js
@@ -1,30 +1,46 @@
+import ultimo_proceso from "./ultimo_proceso.js";
+
 const app = Vue.createApp({
     data(){
         return {
-
+            ultimosProcesos: [],
+            idSeleccionador: sessionStorage.getItem("id_seleccionador") ? sessionStorage.getItem("id_seleccionador") : 2,
+            nombreSeleccionador: "",
+            generoSeleccionador: "",
         }
+    },
+    components: {
+        ultimo_proceso,
     },
     template: `
     <div id="container_bienvenida_seleccionador">
-        <h3>Bienvenid@, NombreSeleccionador</h3>
+        <h3 v-if="generoSeleccionador == 'Hombre'">Bienvenido, {{ nombreSeleccionador }}</h3>
+        <h3 v-else-if="generoSeleccionador == 'Mujer'">Bienvenida, {{ nombreSeleccionador }}</h3>
+        <h3 v-else>Bienvenid@, {{ nombreSeleccionador }}</h3>
         <p>¿Qué quieres hacer?</p>
     </div>
 
     <div id="container_seccion_gestion">
         <div id="bloque_gestion_1" class="bloque_gestion">
-            <div class="imagen_gestion"></div>
+            <div class="imagen_gestion imagen_publicar_proceso">
+                <a href="#"></a>
+            </div>
             <div class="texto_gestion">
                 <a href="#">Publicar proceso</a>
             </div>
         </div>
         <div id="bloque_gestion_2" class="bloque_gestion">
-            <div class="imagen_gestion"></div>
+            <div class="imagen_gestion imagen_gestionar_procesos">
+                <a href="http://next-job.lan/vue/gestionar/procesos"></a>
+            </div>
             <div class="texto_gestion">
                 <a href="#">Gestionar procesos</a>
             </div>
         </div>
         <div id="bloque_gestion_3" class="bloque_gestion">
-            <div class="imagen_gestion"></div>
+            <div class="imagen_gestion imagen_crear_plantilla">
+                <a href="#"></a>
+            </div>
             <div class="texto_gestion">
                 <a href="#">Crear plantilla</a>
             </div>
@@ -36,40 +52,71 @@ const app = Vue.createApp({
             <h3>Últimos procesos</h3>
         </div>
         <div id="subcontainer_ultimos_procesos">
-            @forelse ($ofertas as $oferta)
-                @component('components.ultimo_proceso')
-                    @slot('puesto_trabajo')
-                        {{ $oferta->puesto_trabajo }}
-                    @endslot
 
-                    @slot('ubicacion')
-                        {{ $oferta->ubicacion }}
-                    @endslot
+            <ultimo_proceso v-for="i in ultimosProcesos.length" :key="i" :puesto_trabajo="ultimosProcesos[i - 1][0]" :ubicacion="ultimosProcesos[i - 1][1]" :fecha_publicacion="ultimosProcesos[i - 1][2]" :numero_inscritos="ultimosProcesos[i - 1][3]" :numero_preseleccionados="ultimosProcesos[i - 1][4]" :numero_descartados="ultimosProcesos[i - 1][5]"></ultimo_proceso>
 
-                    @slot('fecha_publicacion')
-                        {{ date('d/m/Y', strtotime($oferta->created_at)) }}
-                    @endslot
-
-                    @slot('numero_inscritos')
-                        {{ $oferta->inscripciones->count() }}
-                    @endslot
-                @endcomponent
-            @empty
-                <div class="container_oferta">
-                    <div class="datos_top">
-                        <div class="titulo_oferta">
-                            <h3>No hay procesos</h3>
-                        </div>
+            <div class="container_oferta" v-if="ultimosProcesos.length == 0">
+                <div class="datos_top">
+                    <div class="titulo_oferta">
+                        <h3>No hay procesos</h3>
                     </div>
                 </div>
-            @endforelse
+            </div>
+
         </div>
     </div>
     `,
-    components: {
-
-    },
     methods: {
+        almacenaNombreYGeneroSeleccionador(arrayDatos){
+            this.nombreSeleccionador = arrayDatos[0]["nombre_seleccionador"];
+            this.generoSeleccionador = arrayDatos[0]["genero_seleccionador"];
+        },
+        almacenaProcesosObtenidos(arrayProcesos){
+            for (let i = 0; i < arrayProcesos.length; i++){
+                this.ultimosProcesos.push([arrayProcesos[i]["puesto_trabajo"], arrayProcesos[i]["ubicacion"], arrayProcesos[i]["fecha_publicacion"], parseInt(arrayProcesos[i]["candidatos_inscritos"]), arrayProcesos[i]["candidatos_preseleccionados"] == "" ? 0 : arrayProcesos[i]["candidatos_preseleccionados"], arrayProcesos[i]["candidatos_descartados"] == "" ? 0 : arrayProcesos[i]["candidatos_descartados"]]);
+            }
+            console.log(this.nombreSeleccionador);
+        },
+        async obtenerNombreYGeneroSeleccionador(){
+            try {
+                let datosSeleccionador = await $.get('http://next-job.lan/build/assets/php/obtener_nombre_genero_seleccionador.php?id_seleccionador=' + this.idSeleccionador);
 
+                if (datosSeleccionador.indexOf("0 resultados") == -1){
+                    let objeto = '{"datosSeleccionador":[' + datosSeleccionador.substring(0, datosSeleccionador.length - 1) + "]}";
+                    objeto = JSON.parse(objeto);
+
+                    console.log(objeto["datosSeleccionador"]);
+
+                    this.almacenaNombreYGeneroSeleccionador(objeto["datosSeleccionador"]);
+                    this.obtenerUltimosProcesos();
+
+                    return objeto;
+                }
+
+            } catch (error) {
+                console.error('Error al hacer la petición', error);
+            }
+        },
+        async obtenerUltimosProcesos(){
+            try {
+                let datosProcesos = await $.get('http://next-job.lan/build/assets/php/obtener_ultimos_procesos.php?id_seleccionador=' + this.idSeleccionador);
+
+                if (datosProcesos.indexOf("0 resultados") == -1){
+                    let objeto = '{"procesos":[' + datosProcesos.substring(0, datosProcesos.length - 1) + "]}";
+                    objeto = JSON.parse(objeto);
+
+                    console.log(objeto["procesos"]);
+
+                    this.almacenaProcesosObtenidos(objeto["procesos"]);
+
+                    return objeto;
+                }
+
+            } catch (error) {
+                console.error('Error al hacer la petición', error);
+            }
+        },
     }
 }).mount('#container');
+
+app.obtenerNombreYGeneroSeleccionador();
---
+++ public/build/assets/php/obtener_ultimos_procesos.php
@@ -0,0 +1,38 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// header('Access-Control-Allow-Origin: *');
+
+$servername = "localhost";
+$username = "laravel";
+$password = "password";
+$dbname = "next_job";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT oferta.puesto_trabajo, oferta.ubicacion, oferta.created_at, CASE WHEN inscritos.candidatos_inscritos IS NULL THEN 0 ELSE inscritos.candidatos_inscritos END AS candidatos_inscritos, candidatos_preseleccionados, candidatos_descartados FROM users INNER JOIN oferta ON users.id = oferta.id_seleccionador
+LEFT OUTER JOIN (SELECT id_oferta, COUNT(id_demandante) AS candidatos_inscritos FROM inscripcion GROUP BY id_oferta) AS inscritos ON oferta.referencia = inscritos.id_oferta LEFT OUTER JOIN candidatos_preseleccionados ON oferta.referencia = candidatos_preseleccionados.id_oferta LEFT OUTER JOIN candidatos_descartados ON oferta.referencia = candidatos_descartados.id_oferta WHERE oferta.id_seleccionador=" . $_GET['id_seleccionador'] . " ORDER BY created_at LIMIT 5";
+
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+  while($row = $result->fetch_assoc()) {
+    echo '{"puesto_trabajo":"' . $row["puesto_trabajo"] . '","ubicacion":"' . $row["ubicacion"]. '","fecha_publicacion":"' . date('d/m/Y', strtotime($row["created_at"])) . '","candidatos_inscritos":"' . $row["candidatos_inscritos"] . '","candidatos_preseleccionados":"' . $row["candidatos_preseleccionados"] . '","candidatos_descartados":"' . $row['candidatos_descartados'] . '"},';
+  }
+} else {
+  echo "'0 resultados'";
+}
+
+$conn->close();
+
+?>
