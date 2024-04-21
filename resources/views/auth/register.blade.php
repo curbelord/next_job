d866e4b471d01df467798a5118b195dee75092a8
Xerach <axercay@gmail.com>
2024-04-21 20:22:09 +0100

    Registro con componente

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,73 +2,84 @@
 
 @section('style')
     <link rel="stylesheet" href="{{ asset('build/assets/css/styleRegistro.css') }}">
+    <script type="module" src="{{ asset('build/assets/js/registro/rol.js') }}"></script>
 @endsection
 
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}" class="registro">
         @csrf
 
-        <h2>Registro</h2>
+        <div id="componente_rol">
 
-        <x-text-input id="nombre" type="text" name="nombre" placeholder="Nombre" :value="old('nombre')" required autofocus autocomplete="nombre" />
-        <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
-
-        <x-text-input id="apellidos" type="apellidos" name="apellidos" placeholder="Apellidos" />
-        <x-input-error :messages="$errors->get('apellidos')" class="mt-2" />
-
-        <select name="genero" id="genero">
-            <option value="" disabled selected>Género</option>
-            <option value="Hombre">Hombre</option>
-            <option value="Mujer">Mujer</option>
-            <option value="Otro">Otro</option>
-        </select>
-        <x-input-error :messages="$errors->get('genero')" class="mt-2" />
-
-        <input type="date" name="fecha_nacimiento" id="fecha_nacimiento" placeholder="Fecha de nacimiento" require>
-        <x-input-error :messages="$errors->get('fecha_nacimiento')" class="mt-2" />
-
-        <input type="text" name="direccion" id="direccion" placeholder="Dirección Postal">
-        <x-input-error :messages="$errors->get('direccion')" class="mt-2" />
-
-        <input type="text" name="telefono" id="telefono" placeholder="Teléfono">
-        <x-input-error :messages="$errors->get('telefono')" class="mt-2" />
-
-        <x-text-input id="email" placeholder="Correo electrónico" type="email" name="email" :value="old('email')" required autocomplete="username" />
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-
-        <x-text-input id="password" type="password" name="password" placeholder="Contraseña" required autocomplete="new-password" />
-        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-
-        <x-text-input id="password_confirmation" type="password" name="password_confirmation" placeholder="Confirmar contraseña" required autocomplete="new-password" />
-        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-
-        <p>
-            ¿Qué tipo de usuario eres?
-        </p>
-
-        <div id="rol">
-            <div>
-                <label for="demandante"> 
+            <h2>
+                ¿Con qué rol te identificas?
+            </h2>
+            
+            <div id="rol">
+                <label for="demandante" id="contenedor_demandante">
+                    <img src="{{ asset('build/assets/img/candidato.svg') }}" alt="Candidato">
                     <input type="radio" name="rol" id="demandante" value="Demandante">
                     Demandante
                 </label>
-            </div>
-            <div>
-                <label for="seleccionador">
+                <label for="seleccionador" id="contenedor_seleccionador">
+                    <img src="{{ asset('build/assets/img/seleccionador.svg') }}" alt="Candidato">
                     <input type="radio" name="rol" id="seleccionador" value="Seleccionador">
                     Seleccionador
                 </label>
             </div>
+
+            <button type="button" id="rol_seleccionado">
+                Siguiente
+            </button>
+
         </div>
 
-        <x-primary-button>
-            {{ __('Registrar') }}
-        </x-primary-button>
+        <div id="formulario_registro">
 
-        <p>
-            ¿Tienes una cuenta?
-            <a href="{{ url('/inicio-de-sesion') }}">Inicia sesión</a>
-        </p>
+            <h2>Registro</h2>
+            
+            <x-text-input id="nombre" type="text" name="nombre" placeholder="Nombre" :value="old('nombre')" required autofocus autocomplete="nombre" />
+            <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
+
+            <x-text-input id="apellidos" type="apellidos" name="apellidos" placeholder="Apellidos" />
+            <x-input-error :messages="$errors->get('apellidos')" class="mt-2" />
+
+            <select name="genero" id="genero">
+                <option value="" disabled selected>Género</option>
+                <option value="Hombre">Hombre</option>
+                <option value="Mujer">Mujer</option>
+                <option value="Otro">Otro</option>
+            </select>
+            <x-input-error :messages="$errors->get('genero')" class="mt-2" />
+
+            <input type="date" name="fecha_nacimiento" id="fecha_nacimiento" placeholder="Fecha de nacimiento" require>
+            <x-input-error :messages="$errors->get('fecha_nacimiento')" class="mt-2" />
+
+            <input type="text" name="direccion" id="direccion" placeholder="Dirección Postal">
+            <x-input-error :messages="$errors->get('direccion')" class="mt-2" />
+
+            <input type="text" name="telefono" id="telefono" placeholder="Teléfono">
+            <x-input-error :messages="$errors->get('telefono')" class="mt-2" />
+
+            <x-text-input id="email" placeholder="Correo electrónico" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
+            <x-text-input id="password" type="password" name="password" placeholder="Contraseña" required autocomplete="new-password" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+
+            <x-text-input id="password_confirmation" type="password" name="password_confirmation" placeholder="Confirmar contraseña" required autocomplete="new-password" />
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+
+            <x-primary-button>
+                {{ __('Registrar') }}
+            </x-primary-button>
+
+            <p>
+                ¿Tienes una cuenta?
+                <a href="{{ url('/inicio-de-sesion') }}">Inicia sesión</a>
+            </p>
+
+        </div>
 
     </form>
 </x-guest-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
         @yield('style')
 
         <!-- Scripts -->
+        <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
         <!-- ANTES ERA ASÍ: ['resources/css/app.css', 'resources/js/app.js'] 
         SE ELIMINAN LOS ESTILOS PREDETERMINADOS DE LARAVEL -->
         @vite('resources/js/app.js')
