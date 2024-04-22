78813ea50019df0180ac5c70feea442333769dac
Xerach <axercay@gmail.com>
2024-04-22 18:00:46 +0100

    Icono de la web

--- resources/views/layouts/plantilla_sin_footer.blade.php
+++ resources/views/layouts/plantilla_sin_footer.blade.php
@@ -12,6 +12,8 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="{{ asset('build/assets/css/styles.css') }}">
         <link rel="stylesheet" href="{{ asset('build/assets/css/style.css') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('build/assets/img/logo_next_job.svg') }}" />
+
         <title>@yield('title')</title>
 
         @yield('style')
