2cef5ee965d40b13c9fcd219394b8db1fa4e469c
Xerach <axercay@gmail.com>
2024-04-23 19:52:52 +0100

    Adición inputs error visibles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,12 +15,14 @@
         <h2>Inicio de sesión</h2>
 
         <x-text-input id="email" type="email" placeholder="Correo electrónico" name="email" :value="old('email')" autofocus autocomplete="username" />
+        <p class="error-mensaje">El campo "Correo electrónico" no puede estar vacío.</p>
 
         <x-text-input id="password"
                         type="password"
                         name="password"
                         placeholder="Contraseña"
                         autocomplete="current-password" />
+        <p class="error-mensaje">El campo "Confirmar contraseña" no puede estar vacío.</p>
 
         <button type="button" id="boton_iniciar_sesion">
             Inicar sesión
