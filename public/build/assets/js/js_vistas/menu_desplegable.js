17dc5f183028525f164d1b42238019a151d97650
Xerach <axercay@gmail.com>
2024-04-27 13:59:29 +0100

    Error Menú Solucionado

--- public/build/assets/js/js_vistas/menu_desplegable.js
+++ public/build/assets/js/js_vistas/menu_desplegable.js
@@ -6,15 +6,17 @@ if (botonMenu){
         let containerListaMenu = document.getElementById("container_lista_menu");
         let listaMenu = document.getElementById("lista_menu");
 
-    if (listaMenu.style.right == "0px"){
-        listaMenu.style.transition = "right 1s";
-        listaMenu.style.right = "-318px";
-        setTimeout(function() {
-            containerListaMenu.style.overflow = "hidden";
-        }, 500);
-    }else{
-        containerListaMenu.style.overflow = "visible";
-        listaMenu.style.transition = "right 500ms";
-        listaMenu.style.right = "0px";
-    }
-});
+        if (listaMenu.style.right == "0px") {
+            listaMenu.style.transition = "right 1s";
+            listaMenu.style.right = "-318px";
+            setTimeout(function() {
+                containerListaMenu.style.overflow = "hidden";
+            }, 500);
+        } else {
+            containerListaMenu.style.overflow = "visible";
+            listaMenu.style.transition = "right 500ms";
+            listaMenu.style.right = "0px";
+        }
+    });
+
+}
